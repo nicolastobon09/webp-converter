659e872704b5266d0119cdb08ce347e96661320b
Nicolas Alvarez <nicolasalvarez@Nicolass-MacBook-Air.local>
2026-09-19 19:05:05 -0500

    fix base

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import react from "@vitejs/plugin-react";
 // Vercel and a GitHub Pages USER/ORG page (username.github.io) both want base: "/".
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/webp-converter/",
 });
